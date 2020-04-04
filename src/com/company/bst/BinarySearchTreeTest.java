2a1cb8ac577c64e068b21583807c1e23992ac904
Prasenjit Chakraborty <cprasenj@thoughtworks.com>
2020-04-04 20:27:22 +0530

    enhance test cases

--- src/com/company/bst/BinarySearchTreeTest.java
+++ src/com/company/bst/BinarySearchTreeTest.java
@@ -17,5 +17,17 @@ class BinarySearchTreeTest {
 
         bst.insert(2);
         assertEquals(3, bst.size());
+
+        BinarySearchTree<Double> bst1 = new BinarySearchTree<>();
+        bst1.insert(10.1);
+        assertEquals(1, bst1.size());
+
+        bst1.insert(5.2);
+        assertEquals(2, bst1.size());
+
+        bst1.insert(2.3);
+        assertEquals(3, bst1.size());
+
+
     }
 }
